43aa55ded86380f101edfe5eb41490b37f653d9d
dbenfon341 <dbenfon341@g.educaand.es>
2025-01-29 19:25:31 +0100

    Main de 4-6 + arreglos varios.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
+import classes.*
+
 fun main() {
-    println("Hello World!")
+    val listaCompras = listOf(
+        Compra(Cliente("Nuria Costa", Domicilio("Calle Las Flores", 355)), 5, 12780.78),
+        Compra(Cliente("Jorge Russo", Domicilio("Mirasol", 218)), 7, 699.0),
+        Compra(Cliente("Nuria Costa", Domicilio("Calle Las Flores", 355)), 7, 532.90),
+        Compra(Cliente("Julián Rodriguez", Domicilio("La Mancha", 761)), 12, 5715.99),
+        Compra(Cliente("Jorge Russo", Domicilio("Mirasol", 218)), 15, 958.0)
+    )
+
+    val repositorioCompras = Repositorio()
+
+    for (compra in listaCompras) {
+        repositorioCompras.agregar(compra)
+    }
+
+    val setDomicilio = repositorioCompras.domicilios()
+
+    for ((i, domicilio) in setDomicilio.withIndex()) {
+        println("Domicilio ${i + 1}: $domicilio")
+    }
 }
--- src/classes/Repositorio.kt
+++ src/classes/Repositorio.kt
@@ -1,19 +1,59 @@
 package classes
 
+/**
+ * Clase que representa un repositorio de compras.
+ * Permite agregar compras y obtener un conjunto de domicilios únicos de los clientes.
+ */
 class Repositorio {
     private val listaCompras = mutableListOf<Compra>()
 
+    /**
+     * Agrega una compra al repositorio.
+     *
+     * @param compra La compra a agregar.
+     */
     fun agregar(compra: Compra) {
         listaCompras.add(compra)
     }
 
+    /**
+     * Agrega una compra al repositorio utilizando la información del cliente, día y total.
+     *
+     * @param cliente El cliente que realizó la compra.
+     * @param dia El día en que se realizó la compra.
+     * @param total El total de la compra.
+     */
     fun agregar(cliente: Cliente, dia: Int, total: Double) {
         listaCompras.add(Compra(cliente, dia, total))
     }
 
+    /**
+     * Agrega una compra al repositorio utilizando el nombre del cliente, su domicilio,
+     * el día y el total de la compra.
+     *
+     * @param nombre El nombre del cliente.
+     * @param calle La calle del domicilio del cliente.
+     * @param numero El número del domicilio del cliente.
+     * @param dia El día en que se realizó la compra.
+     * @param total El total de la compra.
+     */
     fun agregar(nombre: String, calle: String, numero: Int, dia: Int, total: Double) {
 
-        val domicilio = Domicilio(calle, numero)
-        val cliente = Cliente(nombre, domicilio)
+        val domicilio= Domicilio(calle, numero)
+        val cliente= Cliente(nombre, domicilio)
+        listaCompras.add(Compra(cliente, dia, total))
+    }
+
+    /**
+     * Obtiene un conjunto de domicilios únicos de los clientes que realizaron compras.
+     *
+     * @return Un conjunto mutable de domicilios.
+     */
+    fun domicilios(): MutableSet<Domicilio> {
+        val setCompras: MutableSet<Domicilio> = mutableSetOf()
+        for (compra in listaCompras) {
+            setCompras.add(compra.cliente.domicilio)
+        }
+        return setCompras
     }
 }
